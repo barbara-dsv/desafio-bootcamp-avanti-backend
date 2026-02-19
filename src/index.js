84c171a0644074a71f08bcad113187cac2fe770f
Bárbara Lourenço Tavares <babisolua@gmail.com>
2026-02-19 14:29:19 -0300

    :recycle:	refactor: migra index para ES Modules e atualiza imports

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
-const express = require("express");
+import 'dotenv/config';
+import express from "express"
+import routes from "./routes.js";
 
 const app = express();
 
 app.use(express.json());
+app.use(routes);
 
 app.get("/", (req, res) => {
     res.send("Avanti");
 });
-
-
-app.listen(3000, () => {
-    console.log("Servidor rodando na porta 3000");
-});
+app.listen(process.env.PORT || 3000);
