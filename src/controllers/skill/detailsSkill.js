1fb6a8b62326b8f473be790687419fe0094220d4
Bárbara Lourenço Tavares <babisolua@gmail.com>
2026-02-20 11:53:27 -0300

    :sparkles:	feat: implementado funcionalidade de detalhar oferta

---
+++ src/controllers/skill/detailsSkill.js
@@ -0,0 +1,27 @@
+import prisma from "../../PrismaCliente.js";
+
+const detailsSkill = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const skill = await prisma.conhecimento.findUnique({ where: { id: Number(id) } });
+
+        if (!skill) {
+            return res.status(404).json("Conhecimento não encontrado.");
+        };
+
+
+        const detailsSkill = await prisma.conhecimento.findUnique({
+            where: { id: Number(id) },
+            include: {
+                pessoa: true
+            }
+        });
+
+        return res.json(detailsSkill);
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ mensagem: 'Erro interno do servidor.' });
+    }
+};
+
+export default detailsSkill;
--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import updateSkill from "./controllers/skill/updateSkill.js";
 import deleteSkill from "./controllers/skill/deleteSkill.js";
 import updateSkillPerson from "./controllers/skill/updateSkillPerson.js";
 import listSkill from "./controllers/skill/listSkill.js";
+import detailsSkill from "./controllers/skill/detailsSkill.js";
 
 
 const routes = express();
@@ -14,5 +15,6 @@ routes.put("/skill/:id", updateSkill);
 routes.delete("/skill/:id", deleteSkill);
 routes.put("/skill/:id/person/:pessoa_id", updateSkillPerson);
 routes.get("/skill", listSkill);
+routes.get("/skill/details/:id", detailsSkill);
 
 export default routes;
