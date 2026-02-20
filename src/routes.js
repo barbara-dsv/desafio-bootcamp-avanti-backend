c51dbcae0f2b451831f409bf1c06553ef9df4af0
Bárbara Lourenço Tavares <babisolua@gmail.com>
2026-02-19 23:09:36 -0300

    :sparkles:	feat: implentado rotas de deleção e edição de conhecimento

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,16 @@
 import express from "express";
 
 import createSkill from "./controllers/skill/createSkill.js";
+import updateSkill from "./controllers/skill/updateSkill.js";
+import deleteSkill from "./controllers/skill/deleteSkill.js";
+import updateSkillPerson from "./controllers/skill/updateSkillPerson.js";
 
 
 const routes = express();
 
 routes.post("/skill/:pessoa_id", createSkill);
+routes.put("/skill/:id", updateSkill);
+routes.delete("/skill/:id", deleteSkill);
+routes.put("/skill/:id/person/:pessoa_id", updateSkillPerson);
 
 export default routes;
