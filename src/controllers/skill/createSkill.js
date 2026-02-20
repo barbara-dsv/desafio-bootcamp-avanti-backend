bf10aa9d730fd615d57d1d4bcbc6a54c5bba842e
Bárbara Lourenço Tavares <babisolua@gmail.com>
2026-02-20 09:43:16 -0300

    :sparkles: feat: adiciona validação de existência de pessoa ao criar skill

--- src/controllers/skill/createSkill.js
+++ src/controllers/skill/createSkill.js
@@ -5,6 +5,15 @@ const createSkill = async (req, res) => {
     const { pessoa_id } = req.params;
     const { titulo, descricao, categoria, nivel } = req.body;
     try {
+
+        const person = await prisma.pessoa.findUnique({
+            where: { id: Number(pessoa_id) }
+        });
+
+        if (!person) {
+            return res.status(404).json("Pessoa não encontrada.")
+        };
+
         const skill = await prisma.conhecimento.create({
             data: { titulo, descricao, categoria, nivel, pessoa_id: Number(pessoa_id) }
         });
