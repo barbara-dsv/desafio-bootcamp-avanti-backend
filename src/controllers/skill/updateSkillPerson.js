7508d2d77a035ae6f0da4722c561b3805b914289
Bárbara Lourenço Tavares <babisolua@gmail.com>
2026-02-19 23:08:45 -0300

    :sparkles:	feat: implementado rota de associação de cada oferta a uma pessoa específica

---
+++ src/controllers/skill/updateSkillPerson.js
@@ -0,0 +1,26 @@
+import prisma from "../../PrismaCliente.js";
+
+const updateSkillPerson = async (req, res) => {
+    const { id } = req.params;
+    const { pessoa_id } = req.params;
+    try {
+        const skill = await prisma.conhecimento.findUnique({ where: { id: Number(id) } });
+
+        if (!skill) {
+            return response.status(404).json("Conhecimento não encontrado.");
+        };
+
+        const updateSkillPerson = await prisma.conhecimento.update({
+            data: { pessoa_id: Number(pessoa_id) },
+            where: { id: Number(id) }
+        });
+
+        return res.status(200).json(updateSkillPerson);
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ mensagem: 'Erro interno do servidor.' });
+    }
+};
+
+export default updateSkillPerson;
+
