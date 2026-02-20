ef60ca705201d4651d1e2a39d43fe9642f920b37
Bárbara Lourenço Tavares <babisolua@gmail.com>
2026-02-20 09:15:57 -0300

    :sparkles:	feat: implementado funcionalidade de listar conhecimentos

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import createSkill from "./controllers/skill/createSkill.js";
 import updateSkill from "./controllers/skill/updateSkill.js";
 import deleteSkill from "./controllers/skill/deleteSkill.js";
 import updateSkillPerson from "./controllers/skill/updateSkillPerson.js";
+import listSkill from "./controllers/skill/listSkill.js";
 
 
 const routes = express();
@@ -12,5 +13,6 @@ routes.post("/skill/:pessoa_id", createSkill);
 routes.put("/skill/:id", updateSkill);
 routes.delete("/skill/:id", deleteSkill);
 routes.put("/skill/:id/person/:pessoa_id", updateSkillPerson);
+routes.get("/skill", listSkill);
 
 export default routes;
