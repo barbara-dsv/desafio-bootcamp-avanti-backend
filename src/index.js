ec5bcdf9c176c4cf19f6d29ff039fd6d52d2ca16
Bárbara Lourenço Tavares <babisolua@gmail.com>
2026-03-06 12:08:28 -0300

    :boom:	fix: ajusta uso de cors para antes de rotas

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ import express from "express"
 import routes from "./routes.js";
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 app.use(routes);
-app.use(cors)
+
 
 app.get("/", (req, res) => {
     res.send("Avanti");
